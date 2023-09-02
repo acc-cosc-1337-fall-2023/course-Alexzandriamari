b4a0696e5795339cf2983c63882aacee9290682d
Alexzandriamari <69994862+Alexzandriamari@users.noreply.github.com>
2023-09-02 17:36:17 +0000

    Hw1: Muliply_numbers Func & Test Cases

--- src/homework/01_data_types/data_types.cpp
+++ src/homework/01_data_types/data_types.cpp
@@ -3,11 +3,13 @@
 //example
 int add_numbers(int num1, int num2)
 {
-	return num1 + num2;
+return num1 + num2;
 }
 
 //write function code here
-
-
-
-
+int multiply_numbers(int num1) //1B 
+{
+    int num2 = 5;
+    int result = num1 * num2;
+    return result;
+}
